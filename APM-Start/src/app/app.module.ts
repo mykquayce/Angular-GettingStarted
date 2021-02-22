220685e4c4c5d2d8742ea52fe7a23007a3d26a3e
Bob H <myk_quayce@hotmail.com>
2021-02-22 16:38:18 +0000

    8. Building Nested Components

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConvertToSpacesPipe,
     ProductListComponent,
+    StarComponent,
   ],
   imports: [
     BrowserModule,
